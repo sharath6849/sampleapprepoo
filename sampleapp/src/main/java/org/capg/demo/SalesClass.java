a6eed243f4338921f94580faed7d293a85d71d76
sharath6849 <ksharath@LIN65003986.corp.capgemini.com>
2021-09-20 11:00:06 +0530

    Sales class added

---
+++ sampleapp/src/main/java/org/capg/demo/SalesClass.java
@@ -0,0 +1,10 @@
+package org.capg.demo;
+
+public class SalesClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sales class");
+	}
+
+}
